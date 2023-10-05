febbcd2b4f12ea741e5cb39f3f250f8d9a2306e4
Sam Rice <samsrice@gmail.com>
2023-10-05 10:42:40 -0700

    run code formatter

--- src/com/mortgageCalculator/CLI.java
+++ src/com/mortgageCalculator/CLI.java
@@ -3,24 +3,24 @@ package com.mortgageCalculator;
 import java.util.Scanner;
 
 public class CLI {
-  private static Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 
-  public static double readNumber(String prompt) {
-    return scanner.nextDouble();
-  }
-
-  public static double readNumber(String prompt, double min, double max) {
-    double value;
-
-    while (true) {
-      System.out.print(prompt + ": ");
-      value = scanner.nextDouble();
-      if (value >= min && value <= max) {
-        break;
-      } else {
-        System.out.println("Enter a value between " + min + " and " + max);
-      }
+    public static double readNumber(String prompt) {
+        return scanner.nextDouble();
     }
-    return value;
-  }
+
+    public static double readNumber(String prompt, double min, double max) {
+        double value;
+
+        while (true) {
+            System.out.print(prompt + ": ");
+            value = scanner.nextDouble();
+            if (value >= min && value <= max) {
+                break;
+            } else {
+                System.out.println("Enter a value between " + min + " and " + max);
+            }
+        }
+        return value;
+    }
 }
